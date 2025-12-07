6cc7916c3a3fcb9cf7c828af09b7995d6177cc44
Hiro Ito <mobiiii24@gmail.com>
2025-12-07 21:16:16 +0900

    chore: adjust logger, stdout

--- src/cha2hatena/line_message.py
+++ src/cha2hatena/line_message.py
@@ -11,7 +11,7 @@ def line_messenger(content: str, line_access_token: str):
     logger.debug(f"LINEアクセストークン: ... {line_access_token[-5:]}")
 
     if line_access_token:
-        logger.debug("アクセストークンを取得")
+        logger.warning("アクセストークンを取得")
 
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer <{line_access_token}>"}
 
@@ -21,13 +21,13 @@ def line_messenger(content: str, line_access_token: str):
     res = requests.post(URL, headers=headers, json=body)
 
     if res.status_code == 200:
-        logger.debug("✓ LINE通知に成功しました。")
+        logger.warning("✓ LINE通知に成功しました。")
     else:
-        logger.info("LINE通知出来ませんでした。")
-        logger.info(f"ステータスコード：{res.status_code}")
-        res_dict = res.json()
+        logger.error("LINE通知出来ませんでした。詳細は`app.log`を確認してください")
+        logger.error(f"ステータスコード：{res.status_code}")
         try:
+            res_dict = res.json()
             logger.info(f"詳細: {res_dict['message']}")
             logger.info(f"{res_dict['details'][0]['message']}")
-        except Exception as e:
-            logger.info(f"レスポンスを解析できませんでした。詳細: {e}", exc_info=True)
+        except Exception:
+            logger.info("レスポンス内容を解析できませんでした。")
